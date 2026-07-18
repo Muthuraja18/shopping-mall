20493c622922e3a34a06d5a11bea8faf0bb56024
Muthuraja18 <muthurajlingam788@gmail.com>
2026-07-18 14:52:48 +0530

    Refactor App component for authentication flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,111 @@
 import { useState } from "react";
 
-import LandingPage from "./components/LandingPage";
 import Login from "./components/Login";
 import Register from "./components/Register";
-
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
 import HomeView from "./components/HomeView";
-import SearchView from "./components/SearchView";
-import StoresView from "./components/StoresView";
-import ProfileView from "./components/ProfileView";
-import BottomNav from "./components/BottomNav";
+
 
 export default function App() {
+
+
   const [page, setPage] = useState<
-    "landing" | "login" | "register" | "home"
-  >("landing");
+    "login" | "register" | "forgot" | "reset" | "home"
+  >("login");
 
-  const [tab, setTab] = useState<
-    "home" | "search" | "stores" | "profile"
-  >("home");
 
-  const [selectedCategory, setSelectedCategory] = useState<
-    "Fashion" | "Tech" | "Dining" | "Beauty" | "All"
-  >("All");
 
   return (
+
     <>
-      {page === "landing" && (
-        <LandingPage
-          onLogin={() => setPage("login")}
-          onRegister={() => setPage("register")}
-        />
-      )}
 
-      {page === "login" && (
-        <Login
-          onSuccess={() => {
-            setPage("home");
-            setTab("home");
-          }}
-          onRegister={() => setPage("register")}
-        />
-      )}
 
-      {page === "register" && (
-        <Register
-          onLogin={() => setPage("login")}
-        />
-      )}
+      {
+        page === "login" && (
 
-      {page === "home" && (
-        <>
-          {tab === "home" && (
-            <HomeView
-              onSelectStore={(store) => console.log(store)}
-              onSelectCategory={setSelectedCategory}
-              onJoinMembership={() => {}}
-              onOpenConcierge={() => {}}
-              isMember={false}
-              onSwitchTab={setTab}
-            />
-          )}
+          <Login
 
-          {tab === "search" && (
-            <SearchView
-              onSelectStore={(store) => console.log(store)}
-              onOpenConcierge={() => {}}
-              selectedCategory={selectedCategory}
-              onSelectCategory={setSelectedCategory}
-            />
-          )}
+            onSuccess={() => setPage("home")}
 
-          {tab === "stores" && (
-            <StoresView
-              onSelectStore={(store) => console.log(store)}
-            />
-          )}
+            onRegister={() => setPage("register")}
 
-          {tab === "profile" && (
-            <ProfileView />
-          )}
+            onForgotPassword={() => setPage("forgot")}
 
-          <BottomNav
-            active={tab}
-            onChange={setTab}
           />
-        </>
-      )}
+
+        )
+      }
+
+
+
+
+
+      {
+        page === "register" && (
+
+          <Register
+
+            onLogin={() => setPage("login")}
+
+          />
+
+        )
+      }
+
+
+
+
+
+
+      {
+        page === "forgot" && (
+
+          <ForgotPassword
+
+            onBack={() => setPage("login")}
+
+          />
+
+        )
+      }
+
+
+
+
+
+
+
+      {
+        page === "reset" && (
+
+          <ResetPassword
+
+            onSuccess={() => setPage("login")}
+
+          />
+
+        )
+      }
+
+
+
+
+
+
+
+      {
+        page === "home" && (
+
+          <HomeView />
+
+        )
+      }
+
+
+
     </>
+
   );
+
 }
